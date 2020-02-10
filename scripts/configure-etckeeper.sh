13995993b26fd67079e13bf983904de367b8e3ec
Karel Křemel <charles@alembiq.net>
2020-02-10 16:28:30 +0100

    etckeeper config parametrized

--- scripts/configure-etckeeper.sh
+++ scripts/configure-etckeeper.sh
@@ -1,20 +1,26 @@
 #!/usr/bin/env bash
+if [ "$#" -ne 4 ]; then
+	echo "missing parameters: $0 gitlab.server.url" >&2
+	exit 1
+fi
+GITLAB=$1
 
-sudo -i
-apt install etckeeper
 
-ssh-keygen -o -a 100 -t ed25519 -C "$(hostname)-$(date -I)" -f ~/.ssh/$(hostname)-$(date -I)
+sudo apt install etckeeper
 
-echo "Host gitlab.alembiq.net
+sudo ssh-keygen -o -a 100 -t ed25519 -C "$(hostname)-$(date -I)" -f ~/.ssh/$(hostname)-$(date -I)
+
+echo "Host $GITLAB
 IdentityFile ~/.ssh/$(hostname)-$(date -I)
 IdentitiesOnly yes
-user git" >> ~/.ssh/config 
+user git" | sudo tee -a ~/.ssh/config 
 
 echo "###### PUB KEY ######"
 cat ~/.ssh/$(hostname)-$(date -I).pub
 echo "###### save this value to the gitlab repository - settings - repository - deploy keys ######"
 
 cd /etc
-git branch -m $(date -I)
-git remote add origin git@gitlab.alembiq.net:etc/etc-$(hostname)
-git push --set-upstream origin $(date -I)
+sudo git branch -m $(date -I)
+sudo git remote add origin git@$GITLAB:etc/etc-$(hostname)
+sudo git push --set-upstream origin $(date -I)
+sed -i 's/PUSH_REMOTE=\"\"/PUSH_REMOTE=\"origin\"/g /etc/etckeeper/etckeeper.conf 
